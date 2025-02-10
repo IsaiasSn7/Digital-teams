8f99488c0c6ace811811d7e4c606f416e5832f37
IsaiasSn7 <isaiassousa19092000@gmail.com>
2025-02-09 21:25:46 -0300

    carts

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const pesquisaInput = document.getElementById('pesquisa');
 const divCorBorda = document.getElementById('borda-div');
 const IconDePesquisa = document.getElementById('icon-pesquisa');
 
+// focus na cor da borda da div
 pesquisaInput.addEventListener('focus', () =>{
     divCorBorda.classList.add('border-[#C81C6F]')
     })
@@ -9,7 +10,8 @@ pesquisaInput.addEventListener('focus', () =>{
 pesquisaInput.addEventListener('blur', () =>{
     divCorBorda.classList.remove('border-[#C81C6F]')
     })
-
+    
+// focus na cor do icone de pesquisar
 pesquisaInput.addEventListener('focus', () =>{
     IconDePesquisa.setAttribute('color', IconDePesquisa.getAttribute('color') === '#464646'? '#C81C6F' : '#464646')
     })
